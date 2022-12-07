039136e001dad9e85706cbc9d2d6dc41acd2469e
AlexKerb25 <arenzyak3@gmail.com>
2022-12-07 16:22:14 +0600

    Dialogue

---
+++ Answer.gd
@@ -0,0 +1,24 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var effects = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Tween.interpolate_property(self,"modulate:a",0,1,0.7)
+	$Tween.start()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	for x in effects:
+		get_parent().get_parent().effect(x)
+	pass # Replace with function body.
