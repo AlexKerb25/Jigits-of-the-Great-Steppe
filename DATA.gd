cd3699c0e59c9ee697e8ad7e06aae93a27c902d6
AlexKerb25 <arenzyak3@gmail.com>
2022-12-07 12:00:57 +0600

    Settings

--- DATA.gd
+++ DATA.gd
@@ -1,26 +1,19 @@
-extends Control
+extends Node
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var settings = {"fullscreen": false, "locale":"en"}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Save.loadgame()
+	OS.window_fullscreen = settings["fullscreen"]
+	TranslationServer.set_locale(settings["locale"])
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Button3_pressed():
-	get_tree().quit()
-	pass # Replace with function body.
-
-
-func _on_Button2_pressed():
-	get_tree().change_scene("res://Settings.tscn")
-	pass # Replace with function body.
