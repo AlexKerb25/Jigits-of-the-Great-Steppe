7136d61a40d3399a9434b2f47636a4c242636657
AlexKerb25 <arenzyak3@gmail.com>
2022-12-08 14:04:08 +0600

    12332

---
+++ EnemyPanel.gd
@@ -0,0 +1,23 @@
+extends Panel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var hp
+var attackpower = 30
+var cd = 15
+var type = 'dzungar1'
+
+var dzungaranimation = {'dzungar1': 0, 'dzungar2': 0, 'dzungar3': 0, 'dzungar4': 0}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
